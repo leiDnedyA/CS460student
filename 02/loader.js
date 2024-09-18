851174205740f24e1a15da19eafaf0d1684c2bcc
ayden <aydendiel@gmail.com>
2024-09-17 22:58:59 -0400

    feat: added camera cycling

--- 02/loader.js
+++ 02/loader.js
@@ -16,7 +16,7 @@ function download() {
   // create JSON object
   var out = {};
   out['cubes'] = ALL_CUBES;
-  out['camera'] = r.camera.view;
+  out['camera'] = CAMERAS;
 
   // from https://stackoverflow.com/a/30800715
   var dataStr = "data:text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(out));
@@ -55,9 +55,9 @@ function upload(scene) {
 
     }
 
-    // restore camera
-    r.camera.view = new Float32Array(Object.values(loaded['camera']));
-
+    // restore cameras
+    r.camera.view = new Float32Array(Object.values(loaded['camera'][0]));
+    CAMERAS = loaded['camera'].map(camera => new Float32Array(Object.values(camera)));
 
   };
   req.send(null);
